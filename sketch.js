e13fce76ec97556acad9af59e7d9d6fc5e6a8c66
Jeremy-Andrew <97487383+Jeremy-Andrew@users.noreply.github.com>
2022-03-30 17:39:22 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+let img; // Declare variable 'img'.
+
+function setup() {
+  createCanvas(720, 400);
+  img = loadImage('assets/JeremyDailyLogo.png'); // Load the image
+}
+
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
+}
